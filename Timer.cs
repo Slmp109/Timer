3bcd99157823ca7b36657dd8ec35627d40eb54aa
Slmp109 <149095851+Slmp109@users.noreply.github.com>
2023-12-10 17:49:00 +0300

    timer

---
+++ Timer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float minutes;
+    public float seconds;
+
+    // Start is called before the fssssssssssssirst frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Осталось: " + minutes + ":" + seconds);
+        seconds -= Time.deltaTime;
+        if (seconds <= 0) {
+            minutes -= 1;
+            if (minutes <= 0) {
+                int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(sceneIndex);
+            }
+            else
+            {
+                seconds += 60;  
+            }
+        }
+    }
+}
